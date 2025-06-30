a8f14d6b03340c0d7c70c7d125ef71542382d1b1
Kevin Li <kevinli5371@gmail.com>
2025-06-29 23:24:52 -0400

    created landing page and made animation

--- frontend/src/app/landing/page.tsx
+++ frontend/src/app/landing/page.tsx
@@ -4,9 +4,9 @@ export default function Landing() {
     return (
         <div className="main">
             <h1 className="title">Series</h1>
-            <span>meet your AI friend</span>
-            <p>The First AI Social Network</p>
-            <button>Join us</button>
+            <span className="dynamic-text">meet your AI friend</span>
+            <p className="regular-text">The First AI Social Network</p>
+            <button className="join">Join us</button>
         </div>
     );
 }
