e02a26a3bdbbb465a0bd4419fefb446e31976b7c
Kevin Li <kevinli5371@gmail.com>
2025-06-29 21:41:15 -0400

    created next app

---
+++ frontend/src/app/landing/page.tsx
@@ -0,0 +1,12 @@
+import './landing.css';
+
+export default function Landing() {
+    return (
+        <div className="main">
+            <h1 className="title">Series</h1>
+            <span>meet your AI friend</span>
+            <p>The First AI Social Network</p>
+            <button>Join us</button>
+        </div>
+    );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import './globals.css';
+import Landing from './landing/page';
+
+export default function Home() {
+  return (
+    <div className="landing">
+      <Landing />
+    </div>
+  );
+}
